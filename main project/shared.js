9fdbaae26d113e16a85cd74d53b3f0f0f4329cba
khriptonite <68643700+khriptonite@users.noreply.github.com>
2021-05-12 21:06:33 +0300

    Add files via upload

---
+++ main project/shared.js
@@ -0,0 +1,44 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPLanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+console.dir
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPLanButton.length; i++) {
+    selectPLanButton[i].addEventListener('click', function() {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; //This will actually overwrite the complete class list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+if (modalNoButton) {
+    modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display =  'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
